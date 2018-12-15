3fa37294e0fac2f152d1248a5e48280ab6205225
zzc <370531378@qq.com>
2018-12-16 07:25:05 +0800

    添加markdown和画面效果，以后再将同步效果完善，这个搞太久了，做了四天，操！

---
+++ js/text.js
@@ -0,0 +1,8 @@
+var word='hello'
+var n=0
+var clock=setInterval(()=>{
+    n+=1
+    document.body.innnerText=word.substring(0,n)
+    //document.body.innerText=word.slice(0,n)
+    if(n>)
+        })
